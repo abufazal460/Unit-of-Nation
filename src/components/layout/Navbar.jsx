4f2332ef45692a53b04ab84dcba58c3607e7fcca
Abu Fazal <abufazal460@gmail.com>
2026-09-11 16:45:21 +0530

    navbar style change

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -42,13 +42,13 @@ export function Navbar({ currentPath = "/" }) {
             alt="Unite For Nation Logo"
             width="44"
             height="48"
-            className="w-12 h-14 sm:w-13 sm:h-15"
+            className="w-12 h-14 sm:w-19 sm:h-13"
           />
           <div>
             <span className="block text-2xl sm:text-2xl font-heading font-extrabold text-slate-900 tracking-tight group-hover:text-[#2A9D8F] transition-colors">
-              {site.name}
+              {/* {site.name} */}
             </span>
-            <span className="block text-xs text-slate-600 font-mono tracking-wide uppercase font-semibold">
+            <span className="block text-sm sm:text-md font-heading font-extrabold text-slate-900 tracking-tight group-hover:text-[#2A9D8F] transition-colors">
               A Human Rights Organization
             </span>
           </div>
